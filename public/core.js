552ccde1210201e19e915a5beb18ccb426ec1836
julian <julian.gallegos@gmail.com>
2014-12-23 12:32:16 -0800

    added angular controller

--- public/core.js
+++ public/core.js
@@ -1,3 +1,38 @@
 var scotchTodo = angular.module('scotchTodo', []);
 
-function mainController($scope, $http)
+function mainController($scope, $http){
+	$scope.formData = {};
+
+	$http.get('/api/todos')
+		.success(function(data){
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data){
+			console.log('Error: ' + data);
+		});
+
+	$scope.createTodo = function(){
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data){
+				$scope.formData = {}; //this clears out the form so it can enter a new object by setting it to an empty object
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
+
+
+	$scope.deleteTodo = function(id){
+		$http.delete('/api/todos' + id)
+			.success(function(data){
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
+}
